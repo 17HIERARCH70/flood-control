97808b222de14f98958b74696d01fb9084dfba94
17HIERARCH70 <nikitabelekov@gmail.com>
2024-03-23 23:05:08 +0300

    fixed

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,41 +8,39 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
-// AppConfig структура, содержащая всю конфигурацию приложения.
+// AppConfig structure containing the entire application configuration.
 type AppConfig struct {
 	Redis        RedisConfig        `yaml:"redis"`
 	FloodControl FloodControlConfig `yaml:"floodControl"`
 }
 
-// RedisConfig структура для конфигурации Redis сервера.
+// RedisConfig structure for configuring a Redis server.
 type RedisConfig struct {
 	Address  string `yaml:"address"`
 	Password string `yaml:"password,omitempty"`
 	DB       int    `yaml:"db"`
 }
 
-// FloodControlConfig структура для параметров контроля флуда.
+// FloodControlConfig structure for flood control parameters.
 type FloodControlConfig struct {
 	RequestLimit  int `yaml:"requestLimit"`
 	PeriodSeconds int `yaml:"periodSeconds"`
 }
 
-// LoadConfig функция для загрузки конфигурации.
+// LoadConfig function to load the configuration.
 func LoadConfig() (*AppConfig, error) {
 	configPath := fetchConfigPath()
 
-	// Проверяем, указан ли путь к файлу конфигурации
 	if configPath == "" {
 		return nil, fmt.Errorf("path to config file is not provided")
 	}
 
-	// Проверяем, существует ли файл конфигурации
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		return nil, fmt.Errorf("config file does not exist: %s", configPath)
 	}
 
 	var cfg AppConfig
-	// Читаем конфигурацию из файла
+
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
 		return nil, fmt.Errorf("failed to read config: %v", err)
 	}
@@ -50,13 +48,12 @@ func LoadConfig() (*AppConfig, error) {
 	return &cfg, nil
 }
 
-// fetchConfigPath функция для получения пути к файлу конфигурации.
+// fetchConfigPath function to get the path to the configuration file.
 func fetchConfigPath() string {
 	var configPath string
 	flag.StringVar(&configPath, "config", "", "path to the config file")
 	flag.Parse()
 
-	// Если флаг не указан, пробуем получить путь из переменной окружения
 	if configPath == "" {
 		configPath = os.Getenv("CONFIG_PATH")
 	}
